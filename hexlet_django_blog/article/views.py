9b7fd2168049cfc1ee83ac74ae7bb74c27059598
Andrey Dolganov <dolganov2002@mail.ru>
2025-02-13 18:00:49 +0300

    articles template

--- hexlet_django_blog/article/views.py
+++ hexlet_django_blog/article/views.py
@@ -1,5 +1,8 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def index(request):
-    return HttpResponse('article')
+    return render(request, 'articles/index.html', context={
+        'name': 'Articles',
+    })
 # Create your views here.
