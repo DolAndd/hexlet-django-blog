ec27c590d5b20c3a68d8f6793724ff18ae7712e2
Andrey Dolganov <dolganov2002@mail.ru>
2025-02-15 22:44:06 +0300

    динамические маршруты

--- hexlet_django_blog/article/views.py
+++ hexlet_django_blog/article/views.py
@@ -1,8 +1,19 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.views import View
+from django.shortcuts import redirect
+from django.urls import reverse
 
-def index(request):
+
+def index(request, tags, article_id):
     return render(request, 'articles/index.html', context={
-        'name': 'Articles',
+        'tags': tags, 'article_id': article_id
     })
+
+
+class IndexView(View):
+
+    def get(self, request, *args, **kwargs):
+        return redirect(reverse("article", kwargs={'tags': "python", 'article_id': 42}))
+
 # Create your views here.
