46fdf13fe8b445002e3b1ceb2f4d2b0b152fa1f4
Andrey Dolganov <dolganov2002@mail.ru>
2025-02-26 23:11:04 +0300

    add CRUD functions

--- hexlet_django_blog/article/urls.py
+++ hexlet_django_blog/article/urls.py
@@ -3,6 +3,9 @@ from django.urls import path
 from hexlet_django_blog.article import views
 
 urlpatterns = [
-    path('', views.IndexView.as_view()),
-    path('<str:tags>/<int:article_id>/', views.index, name = "article"),
+    path('', views.IndexView.as_view(), name='articles_index'),
+    path('<int:id>/edit/', views.ArticleFormEditView.as_view(), name='articles_update'),
+    path('<int:id>/', views.ArticleView.as_view(), name='show'),
+    path('create/', views.ArticleCreateView.as_view(), name='articles_create'),
+    path('<int:id>/delete/', views.ArticleFormDeleteView.as_view(), name='articles_delete'),
 ]
--- hexlet_django_blog/article/views.py
+++ hexlet_django_blog/article/views.py
@@ -1,19 +1,75 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+# from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
 from django.views import View
 from django.shortcuts import redirect
 from django.urls import reverse
+from hexlet_django_blog.article.models import Article
+from hexlet_django_blog.article.forms import ArticleForm
+from django.contrib import messages
 
 
-def index(request, tags, article_id):
-    return render(request, 'articles/index.html', context={
-        'tags': tags, 'article_id': article_id
-    })
+class ArticleCreateView(View):
+    template_name = 'articles/create.html'
+
+    def get(self, request, *args, **kwargs):
+        form = ArticleForm()
+        return render(request, self.template_name, {'form': form})
+
+    def post(self, request, *args, **kwargs):
+        form = ArticleForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Данные успешно сохранены!')
+            return redirect(reverse('articles_index'))
+        else:
+            messages.error(request, 'Ошибка ввода данных!')
+            return render(request, self.template_name, {'form': form})
 
 
 class IndexView(View):
 
     def get(self, request, *args, **kwargs):
-        return redirect(reverse("article", kwargs={'tags': "python", 'article_id': 42}))
+        articles = Article.objects.all()[:15]
+        return render(request, 'articles/index.html', context={
+            'articles': articles,
+        })
 
+
+class ArticleView(View):
+
+    def get(self, request, *args, **kwargs):
+        article = get_object_or_404(Article, id=kwargs['id'])
+        return render(request, 'articles/show.html', context={
+            'article': article,
+        })
 # Create your views here.
+
+class ArticleFormEditView(View):
+
+    def get(self, request, *args, **kwargs):
+        article_id = kwargs.get('id')
+        article = Article.objects.get(id=article_id)
+        form = ArticleForm(instance=article)
+        return render(request, 'articles/update.html', {'form': form, 'article_id':article_id})
+
+    def post(self, request, *args, **kwargs):
+        article_id = kwargs.get('id')
+        article = Article.objects.get(id=article_id)
+        form = ArticleForm(request.POST, instance=article)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Данные успешно сохранены!')
+            return redirect('articles_index')
+
+        messages.error(request, 'Ошибка ввода данных!')
+        return render(request, 'articles/update.html', {'form': form, 'article_id': article_id})
+
+class ArticleFormDeleteView(View):
+
+    def post(self, request, *args, **kwargs):
+        article_id = kwargs.get('id')
+        article = Article.objects.get(id=article_id)
+        if article:
+            article.delete()
+            messages.success(request, 'Данные успешно удалены!')
+        return redirect('articles_index')
